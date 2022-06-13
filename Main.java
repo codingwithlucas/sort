6be3f1335144e05b65d73c1008957067c9319a88
Lucas Fernando <100543848+codingwithlucas@users.noreply.github.com>
2022-06-13 12:10:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,18 +10,24 @@ public class Main {
         int[] result = {};
 
         // Monstrando o vetor original
+        // Showing the original vector
         System.out.println("----- Vetor não ordenado -----");
+        System.out.println("----- Non-ordered vector -----");
         mostrarVetor(vetor);
 
         // Mostrando o vetor ordenado pela classe Selectionsort
+        // Showing the vector sorted by the Selectionsort algorithm
         Selectionsort alg1 = new Selectionsort();
         result = alg1.ordenar(vetor);
+        Systen.out.println("\n\n----- Vector sorted by Selectionsort -----");
         System.out.println("\n\n----- Vetor ordenado pelo Selectionsort -----");
         mostrarVetor(result);
 
         // Mostrando o vetor ordenado pela class Bubblesort
+        // Showing the vector sorted by the Bubblesort class
         Bubblesort alg2 = new Bubblesort();
         result = alg2.ordenar(vetor);
+        System.out.println("\n\n----- Vector sorted by Bubblesort -----");
         System.out.println("\n\n----- Vetor ordenado pelo Bubblesort ------");
         mostrarVetor(result);
     }
